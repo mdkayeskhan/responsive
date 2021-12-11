13a2bd72cace293a0f12a1393d33ef8d8aba96f0
mdkayes <81983819+mdkayeskhan@users.noreply.github.com>
2021-12-12 01:05:10 +0600

    Add files via upload

---
+++ js/responsive.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+			$('#icon').click(function(){
+				$('ul').toggleClass('show');
+			})
+		})
+$(window).load(function() {
+        $('#slider').nivoSlider();
+    });
